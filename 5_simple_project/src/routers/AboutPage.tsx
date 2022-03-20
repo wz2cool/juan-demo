b6dfbe2ff5fe292cd1cc2e62d800162542d24333
wz2cool <wz2cool@live.cn>
2022-03-20 16:28:13 +0800

    add

---
+++ 5_simple_project/src/routers/AboutPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import RouterNav from "../components/RouterNav";
+
+const AboutPage: React.FC = () => {
+    return (<div>
+        <RouterNav />
+        About
+    </div>)
+}
+
+export default AboutPage;
---
+++ 5_simple_project/src/routers/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RouterNav from "../components/RouterNav";
+
+
+const HomePage: React.FC = () => {
+    return (<div>
+        <RouterNav />
+        Home
+    </div>)
+}
+
+export default HomePage;
