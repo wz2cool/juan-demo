49fb3228beb9c8ecf6dd9ff643c3d4571f6c65a3
wz2cool <wz2cool@live.cn>
2022-03-20 11:15:22 +0800

    init

---
+++ webpack-demo_2/src/index.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+  const divTimer = document.getElementById("div_timer");
+  divTimer.innerHTML = new Date();
+}, 1000);
---
+++ webpack-demo_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "./dist"),
+    },
+    compress: true,
+    port: 8000,
+  },
+};
